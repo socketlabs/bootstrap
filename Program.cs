f15a3c37da04af42e44b6b72e4a957f0dbd5d621
Matt Soler <matt.soler@socketlabs.com>
2021-06-16 21:06:59 -0400

    graviton initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketLabs.Bootstrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
